c87bf6b9c3f3c648fff0df5b5c54d95eed0377a3
yoyoung02 <yoyoung0202@gmail.com>
2023-07-25 15:05:30 +0900

    [0725]백엔드 회원가입/로그인 기능 구현

---
+++ HealthBridge/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from accounts import views
+from accounts.views import *
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+]
