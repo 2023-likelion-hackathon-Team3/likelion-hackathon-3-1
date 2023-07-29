f1d4f62cc274b7fe014952117f13da03ccdc5feb
yoyoung02 <yoyoung0202@gmail.com>
2023-07-29 15:29:23 +0900

    BE 게시판 기능 구현(시간(ex.하루전), 이전/다음글, 사진, 전문가 아이디체크 기능 빼고)

--- HealthBridge/board/views.py
+++ HealthBridge/board/views.py
@@ -7,28 +7,17 @@ def board(request):
     question_list = Board.objects.all()
     return render(request, 'board.html', {'question_list':question_list})
 
-def create(request):
+def write(request):
     if request.method == 'POST':
+        now_user=request.user
         new_question = Board()
         new_question.title = request.POST.get('title')
         new_question.content = request.POST.get('content')
+        new_question.hb_user = now_user
         new_question.save()
         return redirect('board:board-main')
-    return render(request, 'create.html')
+    return render(request, 'write.html')
 
 def detail(request, id):
     list = get_object_or_404(Board, pk=id)
     return render(request, 'detail.html', {'list':list})
-
-def update(request, id):
-    update_list = get_object_or_404(Board, pk=id)
-    
-    if update_list.answer.strip() == '': #전문가답변이 없을 때
-        if request.method == 'POST':
-            update_list.title = request.POST.get('title')
-            update_list.content = request.POST.get('content')
-            update_list.save()
-            return redirect('board:board-detail', update_list.pk)
-        return render(request, 'update.html', {'update_list': update_list})
-    else: #전문가 답변이 존재할 때
-        return redirect('board:board-detail', update_list.pk)
