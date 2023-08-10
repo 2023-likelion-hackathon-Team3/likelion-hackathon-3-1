b03d748b53bd2c07d345c03ea89748f2624a31e6
yoyoung02 <yoyoung0202@gmail.com>
2023-08-10 22:51:09 +0900

    로그아웃 기능 추가

--- HealthBridge/accounts/urls.py
+++ HealthBridge/accounts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('kakao/set_password/', views.set_password, name='set_password'),
-    
+    path('logout/', views.logout, name='logout'),
 ]
